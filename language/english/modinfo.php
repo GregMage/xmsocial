5b8c95073ecbb8cdd5f873a89acb6d0ad351352f
Grégory Mage <gregory.mage@gmail.com>
2019-02-12 21:47:22 +0100

    Structure du module

---
+++ language/english/modinfo.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmsocial module
+ *
+ * @copyright       XOOPS Project (http://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+// The name of this module
+define('_MI_XMSOCIAL_DESC', 'Social management');
+
+// Menu
+define('_MI_XMSOCIAL_MENU_HOME', 'Index');
+define('_MI_XMSOCIAL_MENU_ABOUT', 'About');
+
+// Pref
+define('_MI_XMDOC_XMSOCIAL_HEAD_ADMIN', '<span style="font-size: large;  font-weight: bold;">Administration</span>');
+define('_MI_XMDOC_XMSOCIAL_ITEMPERPAGE', 'Number of items per page in the administration view');
---
+++ xoops_version.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmsocial module
+ *
+ * @copyright       XOOPS Project (http://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+$modversion['dirname']     = basename(__DIR__);
+$modversion['name']        = ucfirst(basename(__DIR__));
+$modversion['version']     = '0.1';
+$modversion['description'] = _MI_XMSOCIAL_DESC;
+$modversion['author']      = 'Grégory Mage (Mage)';
+$modversion['url']         = 'https://github.com/GregMage';
+$modversion['credits']     = 'Mage';
+
+$modversion['help']        = 'page=help';
+$modversion['license']     = 'GNU GPL 2 or later';
+$modversion['license_url'] = 'http://www.gnu.org/licenses/gpl-2.0.html';
+$modversion['official']    = 0;
+$modversion['image']       = 'assets/images/xmsocial_logo.png';
+
+// Menu
+$modversion['hasMain'] = 0;
+
+// Admin things
+$modversion['hasAdmin']    = 1;
+$modversion['system_menu'] = 1;
+$modversion['adminindex']  = 'admin/index.php';
+$modversion['adminmenu']   = 'admin/menu.php';
+
+// Install and update
+//$modversion['onInstall']        = 'include/install.php';
+//$modversion['onUpdate']         = 'include/update.php';
+
+// Tables
+//$modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
+
+//$modversion['tables'][1] = '';
+
+
+// Admin Templates
+$modversion['templates'][] = array('file' => 'xmsocial_admin_.tpl', 'description' => '', 'type' => 'admin');
+
+// User Templates
+$modversion['templates'][] = array('file' => 'xmsocial_.tpl', 'description' => '');
+
+
+
+// Configs
+$modversion['config'] = array();
+
+$modversion['config'][] = array(
+    'name'        => 'break',
+    'title'       => '_MI_XMSOCIAL_PREF_HEAD_ADMIN',
+    'description' => '',
+    'formtype'    => 'line_break',
+    'valuetype'   => 'text',
+    'default'     => 'head',
+);
+
+$modversion['config'][] = array(
+    'name'        => 'admin_perpage',
+    'title'       => '_MI_XMSOCIAL_PREF_ITEMPERPAGE',
+    'description' => '',
+    'formtype'    => 'textbox',
+    'valuetype'   => 'int',
+    'default'     => 15
+);
+
+// About stuff
+$modversion['module_status'] = 'Alpha';
+$modversion['release_date']  = '2019/02/12';
+
+$modversion['developer_lead']      = 'Mage';
+$modversion['module_website_url']  = 'github.com/GregMage';
+$modversion['module_website_name'] = 'github.com/GregMage';
+
+$modversion['min_xoops'] = '2.5.10';
+$modversion['min_php']   = '5.6.31';
